9aa7f5c9932582fffa1e9c34208e68babfab8431
dominaki <dominaki@gmail.com>
2015-07-20 22:56:23 -0500

    meteorologist_1st_draft
    assignment is complete but would like to go back and just make few
    tweaks.  I saw one difference in result between target & my results.

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -9,6 +9,7 @@ class MeteorologistController < ApplicationController
   def street_to_weather
     @street_address = params[:user_street_address]
     url_safe_street_address = URI.encode(@street_address)
+    
 
     # ==========================================================================
     # Your code goes below.
@@ -17,18 +18,22 @@ class MeteorologistController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
-
-
-    @current_temperature = "Replace this string with your answer."
-
-    @current_summary = "Replace this string with your answer."
-
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
-
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@street_address}"
+    @result= open(url).read
+    @parsed_result=JSON.parse(@result) 
+    @longitude = @parsed_result["results"][0]["geometry"]["location"]["lng"]
+    @latitude = @parsed_result["results"][0]["geometry"]["location"]["lat"]
+    
+    url = "https://api.forecast.io/forecast/8c8c63dbae4ec1265ca59794f492b284/#{@latitude},#{@longitude}"
+    @result=open(url).read
+    @parsed_result=JSON.parse(@result)
+    @current_temperature = @parsed_result["currently"]["temperature"]
+    @current_summary = @parsed_result["currently"]["summary"]
+    @summary_of_next_sixty_minutes = @parsed_result["hourly"]["data"][0]["summary"]
+    @summary_of_next_several_hours = @parsed_result["hourly"]["summary"]
+    @summary_of_next_several_days = @parsed_result["daily"]["summary"]
 
     render("street_to_weather.html.erb")
+    
   end
 end
